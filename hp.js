5283b6f8bcf913f9e9d314e6903c589dde4c1f0a
HIMANSHU PARGHi <himanshuparghi.0710@gmail.com>
2025-05-16 18:02:36 +0530

    hp

---
+++ hp.js
@@ -0,0 +1,29 @@
+// "use strict";
+// // console.log(typeof null);
+// // console.log(typeof "hi");
+
+// let score = "33abs";
+// // let score = null;
+// // let score = undefined;
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// // let isIn = 1;
+// let isIn = "";
+// let booleanInIsIn = Boolean(isIn);
+// console.log(booleanInIsIn);
+
+// let nn = 1;
+// let str = String(nn);
+// console.log(typeof str);
+
+
+alert("Hello");
+
+[1, 2].forEach(alert);
+
+
